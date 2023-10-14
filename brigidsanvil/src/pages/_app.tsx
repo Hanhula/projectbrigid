dd40865c6ac67ca219e2f888c75de2a84555c7a1
Hanhula <heatherlouisataylor@gmail.com>
2023-10-14 12:55:46 +1100

    allegely a fix for FA issue

--- brigidsanvil/src/pages/_app.tsx
+++ brigidsanvil/src/pages/_app.tsx
@@ -7,6 +7,13 @@ import persistStoreWrapper, { wrapper } from "@/components/store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import Loading from "@/components/ui/Loading/loading";
 
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
+
 function App({ Component, ...rest }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(rest);
   const { pageProps } = props;
