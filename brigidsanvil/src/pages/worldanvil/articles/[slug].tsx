62cd532eb2907416041b4943abecad6ed538dbd1
Hanhula <heatherlouisataylor@gmail.com>
2023-10-31 22:25:06 +1100

    adding spoiler functionality

--- brigidsanvil/src/pages/worldanvil/articles/[slug].tsx
+++ brigidsanvil/src/pages/worldanvil/articles/[slug].tsx
@@ -10,11 +10,104 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { Button, Collapse } from "react-bootstrap";
 
-const CustomSpoiler: React.FC<{ title: string; content: string }> = ({
+function elementToString(element: DOMNode): string {
+  if (element.type === "text") {
+    //@ts-expect-error
+    return element.data;
+    //@ts-expect-error
+  } else if (element.name === "br") {
+    return "<br>";
+    //@ts-expect-error
+  } else if (element.children && element.children.length > 0) {
+    //@ts-expect-error
+    const childStrings = element.children.map((child, index) => {
+      return elementToString(child);
+    });
+
+    //@ts-expect-error
+    if (element.attribs.href) {
+      //@ts-expect-error
+      return `<${element.name} href="${
+        //@ts-expect-error
+        element.attribs.href
+        //@ts-expect-error
+      }">${childStrings.join("")}</${element.name}>`;
+    } else {
+      //@ts-expect-error
+      return `<${element.name || ""}>${childStrings.join("")}</${
+        //@ts-expect-error
+        element.name || ""
+      }>`;
+    }
+  } else {
+    //@ts-expect-error
+    return `<${element.name || ""}></${element.name || ""}>`;
+  }
+}
+
+const customTransform = (domNode: DOMNode) => {
+  //@ts-expect-error
+  if (domNode.type === "tag" && domNode.name === "customurl") {
+    // @ts-expect-error
+    const reactChildren = domNode.children.map(
+      (childNode: DOMNode, index: number) => {
+        if (childNode.type === "text") {
+          // @ts-expect-error
+          return childNode.data;
+        } else {
+          return null;
+        }
+      }
+    );
+
+    return (
+      // @ts-expect-error
+      <Link href={domNode.attribs.href}>
+        {reactChildren.filter((child: any) => child !== null)}
+      </Link>
+    );
+  }
+
+  //@ts-expect-error
+  if (domNode.type === "tag" && domNode.name === "customspoiler") {
+    //@ts-expect-error
+    const spoilerTitle = domNode.children.find(
+      //@ts-expect-error
+      (child) => child.name && child.name === "button"
+    );
+    //@ts-expect-error
+    const spoilerContent = domNode.children.find(
+      //@ts-expect-error
+      (child) => child.name && child.name === "collapse"
+    );
+
+    if (spoilerTitle && spoilerContent) {
+      return (
+        <CustomSpoiler
+          title={spoilerTitle}
+          content={spoilerContent}
+        ></CustomSpoiler>
+      );
+    }
+  }
+  return domNode; // Return the unchanged node for all other cases
+};
+
+const CustomSpoiler: React.FC<{ title: Element; content: DOMNode }> = ({
   title,
   content,
 }) => {
   const [open, setOpen] = useState(false);
+
+  //@ts-expect-error
+  let titleText = title.children ? title.children[0].data : "Spoiler";
+
+  let spoilerContentHTML = elementToString(content);
+  let parsedContent = parse(spoilerContentHTML, {
+    replace: customTransform,
+  });
+
+  console.log(content);
 
   return (
     <div>
@@ -23,10 +116,10 @@ const CustomSpoiler: React.FC<{ title: string; content: string }> = ({
         aria-controls="spoiler-content"
         aria-expanded={open}
       >
-        {title || "Spoiler"}
+        {titleText || "Spoiler"}
       </Button>
       <Collapse in={open}>
-        <div id="spoiler-content">{content}</div>
+        <div id="spoiler-content">{parsedContent}</div>
       </Collapse>
     </div>
   );
@@ -189,9 +282,26 @@ const ArticlePage: React.FC = () => {
       if (parts.length === 2) {
         const spoilerText = parts[0].trim();
         const spoilerTitle = parts[1].trim();
-        return `<customspoiler title="${spoilerTitle}"> ${spoilerText} </customspoiler>`;
+
+        return `
+        <customspoiler>
+          <button class="btn btn-primary">
+            ${spoilerTitle || "Spoiler"}
+          </button>
+          <collapse>
+            <div id="spoiler-content">${spoilerText}</div>
+          </collapse>
+        </customspoiler>
+      `;
       } else {
-        return `<customspoiler title=""> ${content} </customspoiler>`;
+        return `        <customspoiler>
+        <button class="btn btn-primary">
+          "Spoiler"
+        </button>
+        <collapse>
+          <div id="spoiler-content">${parts[0]}</div>
+        </collapse>
+      </customspoiler>`;
       }
     },
   });
@@ -199,8 +309,6 @@ const ArticlePage: React.FC = () => {
   const article = currentArticles.find(
     (article: Article) => article.id === slug
   );
-
-  console.log(article);
 
   if (!article) {
     return <div>Article not found</div>;
@@ -230,63 +338,6 @@ const ArticlePage: React.FC = () => {
 
   preprocessedContent = replaceLinks(preprocessedContent);
   let parsed = parser.parse(preprocessedContent);
-
-  const customTransform = (domNode: DOMNode) => {
-    // @ts-expect-error
-    if (domNode.type === "tag" && domNode.name === "customurl") {
-      // @ts-expect-error
-      const reactChildren = domNode.children.map(
-        (childNode: DOMNode, index: number) => {
-          if (childNode.type === "text") {
-            // @ts-expect-error
-            return childNode.data;
-          } else {
-            return null;
-          }
-        }
-      );
-
-      return (
-        // @ts-expect-error
-        <Link href={domNode.attribs.href}>
-          {reactChildren.filter((child: any) => child !== null)}
-        </Link>
-      );
-    }
-
-    // else if (domNode.type === "tag" && domNode.name === "customspoiler") {
-    //   const spoilerTitle = domNode.attribs.title;
-
-    //   const [open, setOpen] = useState(false);
-
-    //   const parsedSpoilerText = domNode.children.map((childNode) => {
-    //     if (childNode.type === "text") {
-    //       return childNode.data;
-    //     } else if (childNode.type === "tag") {
-    //       // If it's a tag, recursively parse it with the same customTransform function
-    //       return customTransform(childNode);
-    //     } else {
-    //       return null;
-    //     }
-    //   });
-
-    //   return (
-    //     <div>
-    //       <Button
-    //         onClick={() => setOpen(!open)}
-    //         aria-controls="spoiler-content"
-    //         aria-expanded={open}
-    //       >
-    //         {spoilerTitle || "Spoiler"}
-    //       </Button>
-    //       <Collapse in={open}>
-    //         <div id="spoiler-content">{parsedSpoilerText}</div>
-    //       </Collapse>
-    //     </div>
-    //   );
-    // }
-    return domNode; // Return the unchanged node for all other cases
-  };
 
   const parstext = parse(parsed, {
     replace: customTransform,
