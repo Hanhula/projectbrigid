c176672bcd981bbb9b2de3e6cce64a90172de698
Hanhula <heatherlouisataylor@gmail.com>
2023-11-18 01:38:08 +1100

    adding mentions logic, adding saving to WA

--- brigidsanvil/src/components/ui/ArticleEdit/EditComponents/character-edit.tsx
+++ brigidsanvil/src/components/ui/ArticleEdit/EditComponents/character-edit.tsx
@@ -4,18 +4,23 @@ import { WorldAnvilEditor } from "../editor";
 const CharacterEdit = ({ article }: { article: Person }) => {
   return (
     <div>
+      <h1>{article.title}</h1>
+      <br />
       <WorldAnvilEditor
         fieldIdentifier="content"
         id={article.id}
         existingContent={article.content!}
       />
-
+      <br />
+      <h2>Physical Description</h2>
+      <h3>Body Features</h3>
       <WorldAnvilEditor
         fieldIdentifier="bodyFeatures"
         id={article.id}
         existingContent={article.bodyFeatures!}
       />
-
+      <br />
+      <h3>Facial Features</h3>
       <WorldAnvilEditor
         fieldIdentifier="facialFeatures"
         id={article.id}
