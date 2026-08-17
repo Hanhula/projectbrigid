6db4a5d2640efb76302eb95caba8a34595c92754
Hanhula <heatherlouisataylor@gmail.com>
2026-08-17 21:23:10 +1000

    update: plot editor

--- brigidsanvil/src/components/ui/ArticleEdit/ArticleComponents/article-edit-page-registry.ts
+++ brigidsanvil/src/components/ui/ArticleEdit/ArticleComponents/article-edit-page-registry.ts
@@ -5,15 +5,16 @@ import DocumentEdit from "./document-edit";
 import EthnicityEdit from "./ethnicity-edit";
 import FormationEdit from "./formation-edit";
 import ItemEdit from "./item-edit";
-import LawEdit from "./law-edit";
 import LandmarkEdit from "./landmark-edit";
 import LanguageEdit from "./language-edit";
+import LawEdit from "./law-edit";
 import LocationEdit from "./location-edit";
 import MaterialEdit from "./material-edit";
 import MilitaryConflictEdit from "./militaryconflict-edit";
 import MythEdit from "./myth-edit";
 import OrganisationEdit from "./organisation-edit";
 import ProseEdit from "./prose-edit";
+import PlotEdit from "./plot-edit";
 import RankEdit from "./rank-edit";
 import RitualEdit from "./ritual-edit";
 import SpellEdit from "./spell-edit";
@@ -27,14 +28,15 @@ import * as documentRegistry from "./document-edit-registry";
 import * as ethnicityRegistry from "./ethnicity-edit-registry";
 import * as formationRegistry from "./formation-edit-registry";
 import * as itemRegistry from "./item-edit-registry";
-import * as lawRegistry from "./law-edit-registry";
 import * as landmarkRegistry from "./landmark-edit-registry";
 import * as languageRegistry from "./language-edit-registry";
+import * as lawRegistry from "./law-edit-registry";
 import * as locationRegistry from "./location-edit-registry";
 import * as materialRegistry from "./material-edit-registry";
 import * as militaryConflictRegistry from "./militaryconflict-edit-registry";
 import * as mythRegistry from "./myth-edit-registry";
 import * as organisationRegistry from "./organisation-edit-registry";
+import * as plotRegistry from "./plot-edit-registry";
 import * as proseRegistry from "./prose-edit-registry";
 import * as rankRegistry from "./rank-edit-registry";
 import * as ritualRegistry from "./ritual-edit-registry";
@@ -78,9 +80,9 @@ export const articleEditPageRegistry: Record<
   Ethnicity: { Component: EthnicityEdit, fields: ethnicityRegistry },
   Formation: { Component: FormationEdit, fields: formationRegistry },
   Item: { Component: ItemEdit, fields: itemRegistry },
-  Law: { Component: LawEdit, fields: lawRegistry },
   Landmark: { Component: LandmarkEdit, fields: landmarkRegistry },
   Language: { Component: LanguageEdit, fields: languageRegistry },
+  Law: { Component: LawEdit, fields: lawRegistry },
   Location: { Component: LocationEdit, fields: locationRegistry },
   Material: { Component: MaterialEdit, fields: materialRegistry },
   MilitaryConflict: {
@@ -90,6 +92,7 @@ export const articleEditPageRegistry: Record<
   Myth: { Component: MythEdit, fields: mythRegistry },
   Organization: { Component: OrganisationEdit, fields: organisationRegistry },
   Person: { Component: CharacterEdit, fields: characterRegistry },
+  Plot: { Component: PlotEdit, fields: plotRegistry },
   Prose: { Component: ProseEdit, fields: proseRegistry },
   Rank: { Component: RankEdit, fields: rankRegistry },
   Ritual: { Component: RitualEdit, fields: ritualRegistry },
